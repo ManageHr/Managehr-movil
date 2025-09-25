c1307b74b6808e763117b14c263b7f7a75d535b6
piter41242 <piter41242@gmail.com>
2025-09-25 16:11:49 -0500

    hojas de vida parte 2

--- app/src/main/java/com/example/appinterface/Api/ApiServicesKotlin.kt
+++ app/src/main/java/com/example/appinterface/Api/ApiServicesKotlin.kt
@@ -2,6 +2,7 @@ package com.example.appinterface.Api
 
 import com.example.appinterface.Models.EstudiosDto
 import com.example.appinterface.Models.ExperienciaDto
+import com.example.appinterface.Models.HojaDeVidaDto
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -19,5 +20,11 @@ interface ApiServicesKotlin {
 
     @POST("api/estudios")
     fun crearEstudios(@Body estudios: EstudiosDto): Call<Void>
+
+    @GET("api/hojas-de-vida")
+    fun obtenerHojasDeVida(): Call<List<HojaDeVidaDto>>
+
+    @POST("api/hojas-de-vida")
+    fun crearHojasDeVida(@Body hojavida: HojaDeVidaDto): Call<Void>
 
 }
